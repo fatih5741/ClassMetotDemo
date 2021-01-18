285caf05095b1da58172b88d63b3d529e145a8ba
Fatih Demir <fatihdmr.1057@gmail.com>
2021-01-18 13:01:03 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void musteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Id: " + musteri.musteriId);
+            Console.WriteLine("Müşteri Adı: " + musteri.musteriAdi);
+            Console.WriteLine("Müşteri Soyadı: " + musteri.musteriSoyadi);
+            Console.WriteLine("Müşteri Sınıfı: " + musteri.musteriSinifi);
+            Console.WriteLine("Müşteri Tel: " + musteri.musteriTel);
+            Console.WriteLine("Müşteriniz Eklendi");
+        }
+
+        public void musteriSil(Musteri musteri)
+        {
+
+            Console.WriteLine("Müşteri Id: " + musteri.musteriId);
+            Console.WriteLine("Müşteri Adı: " + musteri.musteriAdi);
+            Console.WriteLine("Müşteri Soyadı: " + musteri.musteriSoyadi);
+            Console.WriteLine("Müşteriniz Silindi");
+        }
+
+
+        public void musteriListele (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Id: " + musteri.musteriId);
+            Console.WriteLine("Müşteri Adı: " + musteri.musteriAdi);
+            Console.WriteLine("Müşteri Soyadı: " + musteri.musteriSoyadi);
+            Console.WriteLine("Müşteri Sınıfı: " + musteri.musteriSinifi);
+            Console.WriteLine("Müşteri Tel: " + musteri.musteriTel);
+            Console.WriteLine("Müşteriniz Listelendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+
+            musteri1.musteriId = 1;
+            musteri1.musteriAdi = "Fatih";
+            musteri1.musteriSoyadi = "Demir";
+            musteri1.musteriSinifi = "Gold";
+            musteri1.musteriTel = "541 125 41 57";
+
+            Musteri musteri2 = new Musteri();
+
+            musteri2.musteriId = 2;
+            musteri2.musteriAdi = "Ercan";
+            musteri2.musteriSoyadi = "Öztürk";
+            musteri2.musteriSinifi = "Standart";
+            musteri2.musteriTel = "541 125 52 60";
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.musteriEkle(musteri);
+               
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.musteriSil(musteri);
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.musteriListele(musteri);
+            }
+        }
+    }
+}
